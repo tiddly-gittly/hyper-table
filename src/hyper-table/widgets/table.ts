7470b9d0f0918a246f33cc3e66be9d8e722bdaa2
lin onetwo <linonetwo012@gmail.com>
2024-05-07 23:22:27 +0800

    feat: add pivot table

---
+++ src/hyper-table/widgets/table.ts
@@ -0,0 +1,169 @@
+/* eslint-disable no-new-func */
+/* eslint-disable @typescript-eslint/no-implied-eval */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+import { widget as Widget } from '$:/core/modules/widgets/widget.js';
+import { ListTable, ListTableConstructorOptions, PivotTable, PivotTableConstructorOptions, themes, TYPES } from '@visactor/vtable';
+import { IColumnDimension, IIndicator } from '@visactor/vtable/es/ts-types';
+import { IChangedTiddlers, ITiddlerFields } from 'tiddlywiki';
+
+class ListTableWidget extends Widget {
+  tableInstance?: ListTable | PivotTable;
+
+  refresh(_changedTiddlers: IChangedTiddlers) {
+    return false;
+  }
+
+  render(parent: Element, nextSibling: Element) {
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+    const containerElement = this.getContainerElement();
+    const option: ListTableConstructorOptions = {
+      container: containerElement,
+      ...this.getCommonOptions(),
+      records: this.getRecords(),
+      columns: this.getListColumns(),
+      ...(this.getOtherOptionFromString() ?? {}) as ListTableConstructorOptions,
+    };
+    this.tableInstance = new ListTable(option);
+
+    parent.insertBefore(containerElement, nextSibling);
+    this.domNodes.push(containerElement);
+  }
+
+  protected getContainerElement() {
+    const height = this.getAttribute('height') || '400px';
+    const width = this.getAttribute('width') || '100%';
+    const containerElement = $tw.utils.domMaker('p', {
+      class: 'tc-hyper-table tc-hyper-table-list-table',
+      style: {
+        height,
+        width,
+      },
+    });
+    return containerElement;
+  }
+
+  protected getCommonOptions() {
+    const widthMode = this.getAttribute('widthMode') as 'standard' | 'adaptive' | 'autoWidth' | undefined || 'standard';
+    const isDarkMode = $tw.wiki.getTiddler($tw.wiki.getTiddlerText('$:/palette') ?? '')?.fields?.['color-scheme'] === 'dark';
+    const lightTheme = this.getAttribute('lightTheme') as 'DEFAULT' || 'DEFAULT';
+    const darkTheme = this.getAttribute('darkTheme') as 'DARK' || 'DARK';
+    return {
+      widthMode,
+      // eslint-disable-next-line import/namespace
+      theme: isDarkMode ? themes[darkTheme] : themes[lightTheme],
+    };
+  }
+
+  removeChildDomNodes(): void {
+    if (this.tableInstance !== undefined) {
+      this.tableInstance = undefined;
+    }
+    super.removeChildDomNodes();
+  }
+
+  protected getRecords(): unknown[] | undefined {
+    const filter = this.getAttribute('filter');
+    // remove padding new lines, so it won't change line after `return` in the `new Function`, which cause return undefined.
+    const recordsString = this.getAttribute('records')?.trim?.();
+    let records: unknown[] | undefined = [];
+    if (filter) {
+      records = this.wiki.filterTiddlers(filter).map((title) => $tw.wiki.getTiddler(title)?.fields).filter((item): item is ITiddlerFields => item !== undefined);
+    } else if (recordsString) {
+      try {
+        const parsedRecords = new Function(
+          `return ${recordsString}`,
+        )() as unknown[] | undefined;
+        if (parsedRecords !== undefined) {
+          records = parsedRecords;
+        }
+      } catch (error) {
+        console.error(`hyper-table list-table failed to parse records\n\n${recordsString}`, error);
+      }
+    }
+    return records;
+  }
+
+  private getListColumns(): TYPES.ColumnsDefine | undefined {
+    const columnsString = this.getAttribute('columns')?.trim?.();
+    let columns: TYPES.ColumnsDefine | undefined = [{ field: 'title', title: 'Title', width: 'auto' }];
+    if (columnsString) {
+      try {
+        const parsedColumns = new Function(
+          `return ${columnsString}`,
+        )() as TYPES.ColumnsDefine | undefined;
+        if (parsedColumns !== undefined) {
+          columns = parsedColumns;
+        }
+      } catch (error) {
+        console.error(`hyper-table list-table failed to parse columns\n\n${columnsString}`, error);
+      }
+    }
+    return columns;
+  }
+
+  protected getOtherOptionFromString(): unknown {
+    const optionsString = this.getAttribute('options')?.trim?.();
+    if (optionsString) {
+      try {
+        const parsedOption = new Function(
+          `return ${optionsString}`,
+        )() as unknown;
+        return parsedOption;
+      } catch (error) {
+        console.error(`hyper-table list-table failed to parse options\n\n${optionsString}`, error);
+      }
+    }
+  }
+}
+
+class PivotTableWidget extends ListTableWidget {
+  render(parent: Element, nextSibling: Element) {
+    this.parentDomNode = parent;
+    this.computeAttributes();
+    this.execute();
+    const containerElement = this.getContainerElement();
+    const option: PivotTableConstructorOptions = {
+      container: containerElement,
+      ...this.getCommonOptions(),
+      records: this.getRecords(),
+      columns: this.getPivotOption<IColumnDimension[]>('columns', ['tags']),
+      rows: this.getPivotOption<TYPES.IRowDimension[]>('rows', ['title']),
+      indicators: this.getPivotOption<IIndicator[]>('indicators', ['created', 'modified']),
+      ...(this.getOtherOptionFromString() ?? {}) as PivotTableConstructorOptions,
+    };
+    this.tableInstance = new PivotTable(option);
+
+    parent.insertBefore(containerElement, nextSibling);
+    this.domNodes.push(containerElement);
+  }
+
+  private getPivotOption<T extends IIndicator[] | string[] | undefined>(field: string, defaultValue: string[]): T;
+  private getPivotOption<T extends IColumnDimension[] | string[] | undefined>(field: string, defaultValue: string[]): T;
+  private getPivotOption<T extends TYPES.IRowDimension[] | string[] | undefined>(field: string, defaultValue: string[]): T {
+    const columnsString = this.getAttribute('columns')?.trim?.();
+    let columns = defaultValue as T;
+    if (columnsString) {
+      try {
+        const parsedOptions = new Function(
+          `return ${columnsString}`,
+        )() as T;
+        if (parsedOptions !== undefined) {
+          columns = parsedOptions;
+        }
+      } catch (error) {
+        console.error(`hyper-table list-table failed to parse columns\n\n${columnsString}`, error);
+      }
+    }
+    return columns;
+  }
+}
+
+declare let exports: {
+  ListTableWidget: typeof ListTableWidget;
+  PivotTableWidget: typeof PivotTableWidget;
+};
+exports.ListTableWidget = ListTableWidget;
+exports.PivotTableWidget = PivotTableWidget;
--- src/hyper-table/widgets/widget-loader.js
+++ src/hyper-table/widgets/widget-loader.js
@@ -7,8 +7,9 @@
   // separate the widget from the exports here, so we can skip the require of react code if `!$tw.browser`. Those ts code will error if loaded in the nodejs side.
   // fixes `Error executing boot module $:/plugins/linonetwo/hyper-table/test-widget.js: ReferenceError: EventTarget is not defined`
   try {
-    const { ListTableWidget } = require('$:/plugins/linonetwo/hyper-table/widgets/list-table.js');
+    const { ListTableWidget, PivotTableWidget } = require('$:/plugins/linonetwo/hyper-table/widgets/list-table.js');
     exports['list-table'] = ListTableWidget;
+    exports['pivot-table'] = PivotTableWidget;
   } catch (error) {
     console.error('Error loading hyperTable widget', error);
   }
