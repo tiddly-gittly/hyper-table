1dc6e1989ea2a2519d926ebe878a0a0b71a6dbd5
lin onetwo <linonetwo012@gmail.com>
2025-03-25 16:03:15 +0800

    feat: clear button

--- src/hyper-table/widgets/search-bar/searchBar.ts
+++ src/hyper-table/widgets/search-bar/searchBar.ts
@@ -6,11 +6,23 @@ export function searchBar(search: SearchComponent, containerElement: HTMLElement
     class: 'tc-hyper-table-search-container',
   });
 
+  const inputContainer = $tw.utils.domMaker('div', {
+    class: 'tc-hyper-table-input-container',
+  });
+  searchContainer.append(inputContainer);
+
   const input = $tw.utils.domMaker('input', {
     class: 'tc-hyper-table-search-input',
     attributes: { tabindex: '0' },
   });
-  searchContainer.append(input);
+  inputContainer.append(input);
+  
+  const clearButton = $tw.utils.domMaker('button', {
+    class: 'tc-hyper-table-clear-btn tc-hyper-table-hidden',
+  });
+  clearButton.innerText = '×';
+  clearButton.title = $tw.wiki.getTiddlerText('$:/language/Shortcuts/Input/Cancel/Hint') ?? 'Clear';
+  inputContainer.append(clearButton);
 
   const resultCount = $tw.utils.domMaker('span', {
     class: 'tc-hyper-table-search-result-count',
@@ -38,10 +50,24 @@ export function searchBar(search: SearchComponent, containerElement: HTMLElement
   nextButton.title = $tw.wiki.getTiddlerText('$:/language/Shortcuts/Input/Down/Hint') ?? 'Next';
   searchContainer.append(nextButton);
 
-  // 辅助函数：更新搜索结果计数
   const updateResultCount = (searchResult: ReturnType<SearchComponent['search']>) => {
     resultCount.innerText = searchResult.results.length === 0 ? '0/0' : `${searchResult.index + 1}/${searchResult.results.length}`;
   };
+
+  const clearSearch = () => {
+    input.value = '';
+    search.clear();
+    resultCount.innerText = '0/0';
+    clearButton.classList.add('tc-hyper-table-hidden'); // 清除后隐藏按钮
+  };
+
+  input.addEventListener('input', () => {
+    if (input.value.length > 0) {
+      clearButton.classList.remove('tc-hyper-table-hidden');
+    } else {
+      clearButton.classList.add('tc-hyper-table-hidden');
+    }
+  });
 
   searchButton.addEventListener('click', () => {
     updateResultCount(search.search(input.value));
@@ -55,11 +81,11 @@ export function searchBar(search: SearchComponent, containerElement: HTMLElement
         updateResultCount(search.search(input.value));
       }
     } else if (event.key === 'Escape') {
-      input.value = '';
-      search.clear();
-      resultCount.innerText = '0/0';
+      clearSearch();
     }
   });
+
+  clearButton.addEventListener('click', clearSearch);
 
   previousButton.addEventListener('click', () => {
     if (search?.queryResult?.length > 0) {
